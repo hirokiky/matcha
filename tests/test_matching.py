011baafb9549cbf792ca4755175a3d76698c5472
hirokiky <hirokiky@gmail.com>
2013-10-05 08:57:57 +0900

    Added lacked tests for Matching class

--- tests/test_matching.py
+++ tests/test_matching.py
@@ -7,6 +7,12 @@ def target_class():
     return Matching
 
 
+def get_environ(environ={}):
+    from wsgiref.util import setup_testing_defaults
+    setup_testing_defaults(environ)
+    return environ
+
+
 class DummyPatternTemplate(object):
     def __init__(self, return_value):
         self.return_value = return_value
@@ -43,3 +49,49 @@ def test_getitem_not_matched(target_class):
     with pytest.raises(NotMatched):
         target['path_info']
     assert dummy_pattern_template.called_with == 'path_info'
+
+
+def test_call(target_class):
+    target = target_class('/members/ritsu', 'dummy_case')
+    environ = get_environ({'PATH_INFO': '/members/ritsu',
+                           'SCRIPT_NAME': '/htt/'})
+
+    actual = target(environ)
+
+    assert actual[0] == 'dummy_case'
+    assert actual[1] == {}
+    assert environ['PATH_INFO'] == ''
+    assert environ['SCRIPT_NAME'] == '/htt/members/ritsu'
+
+
+def test_add(target_class):
+    target1 = target_class(matching_records=['ritsu'])
+    target2 = target_class(matching_records=['mio'])
+
+    actual = target1 + target2
+
+    assert actual.matching_records == ['ritsu', 'mio']
+
+
+def test_reverse_matched(target_class):
+    target = target_class('/members/{member}', 'dummy_case', 'member_detail')
+
+    actual = target.reverse('member_detail', member='ritsu')
+
+    assert actual == '/members/ritsu'
+
+
+def test_reverse_not_matched(target_class):
+    from matcha import NotReversed
+    target = target_class('/members/ritsu', 'dummy_case', 'ritsu')
+
+    with pytest.raises(NotReversed):
+        target.reverse('mio')
+
+
+def test_reverse_wrong_urlarg(target_class):
+    from matcha import NotReversed
+    target = target_class('/members/{member}', 'dummy_case', 'member_detail')
+
+    with pytest.raises(NotReversed):
+        target.reverse('member_detail', instrument='drum')
