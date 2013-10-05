cefc2578e92d62c29cde2c430e58268d62d854db
hirokiky <hirokiky@gmail.com>
2013-10-05 10:17:02 +0900

    Added tests for dispatcher

---
+++ tests/test_dispatcher.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+@pytest.fixture
+def target():
+    from matcha import dispatcher
+    return dispatcher
+
+
+def test_without_args(target):
+    with pytest.raises(ValueError):
+        target()
+
+
+def test_with_args(target):
+    actual = target(['Ho-Kago'], ['Tea'], ['Time'])
+
+    assert actual == ['Ho-Kago', 'Tea', 'Time']
